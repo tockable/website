73c05721eb76ab8a387b706fd4c63f85429614ef
Tockable <tockablexyz@gmail.com>
2024-03-19 20:22:07 +0330

    IPFS fixedd

--- actions/ipfs/uploadFileToIpfs.js
+++ actions/ipfs/uploadFileToIpfs.js
@@ -1,8 +1,8 @@
-"use server";
 import { NFTStorage, File } from "nft.storage";
 
 const MAX_RETRIES = 6;
-const NFT_STORAGE_KEY = process.env.NFT_STORAGE_KEY;
+const NFT_STORAGE_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDc4MDdmYTI0YTY5QUFmZTg4RTk0OGM1NzVkY0Y4NDU2OTY2YTFBRTEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5NjIwOTUxNTUxNywibmFtZSI6InRvY2thYmxleHl6In0.R_j_1Coce7xXfNfYz8y-NqSpFMQkOpt3y8s_3V6VF40";
 
 export default async function storeFileToIpfs(
   buffer,
@@ -18,7 +18,7 @@ export default async function storeFileToIpfs(
       existance: false,
       message: err.message,
     };
-
+  console.log("wehre");
   try {
     const { cid, existance } = await _store(
       buffer,
@@ -29,7 +29,7 @@ export default async function storeFileToIpfs(
 
     if (cid === "" || cid === null || cid === undefined)
       throw new Error("invalid cid");
-    
+
     return { success: true, cid, existance };
   } catch (err) {
     return await storeFileToIpfs(buffer, mimeType, fileName, retries - 1, err);
